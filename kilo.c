7d2939c79d72759552d7bc99603bf4a8eb3b1893
Abeeku Bondzie <kwekubondzie@gmail.com>
2017-05-07 16:21:22 -0400

    macron

--- kilo.c
+++ kilo.c
@@ -1,32 +1,50 @@
+/** Clude **/
 #include <ctype.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
+/** Clude **/
 
+/** storage **/
 struct termios orig_termios;
+/** storage  **/
+
+
+void crashingthisplane(const char *s){
+  perror(s);
+  exit(1);
+}
 
 void disableRawMode(){
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+ if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+   crashingthisplane("tcsetattr");
 }
 void enableRawMode() {
 
-  tcgetattr(STDIN_FILENO, &orig_termios);
+ if(tcgetattr(STDIN_FILENO, &orig_termios) == -1) crashingthisplane("tcsetattr");
   atexit(disableRawMode);
   struct termios raw = orig_termios;
-  raw.c_lflag &= ~(ECHO | ICANON);
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+  raw.c_iflag &= ~(BRKINT | INPCK | ISTRIP | IXON | ICRNL);
+  raw.c_oflag &= ~(OPOST);
+  raw.c_lflag &= ~(ECHO | ICANON| IEXTEN | ISIG);
+  raw.c_cc[VMIN] = 0;
+  raw.c_cc[VTIME] = 1;
+  if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) crashingthisplane("tcsetattr");
 }
 int main() {
   enableRawMode();
 
-  char c;
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q'){
+  while (1){
+    char c = '\0';
+    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) crashingthisplane ("read my lips");
     if(iscntrl(c)) {
-      printf("%d\n", c);
+      printf("%d\r\n", c);
     } else {
-      printf("%d ('%c')\n",c,c);
+      printf("%d ('%c')\r\n",c,c);
     }
+    if (c == 'q') break;
   }
   return 0;
 }
