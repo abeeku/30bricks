3da77ff9841b333a17bbe6e7541da4d9d9114ad1
Abeeku Bondzie <kwekubondzie@gmail.com>
2017-05-07 15:53:37 -0400

    pattycake

--- kilo.c
+++ kilo.c
@@ -1,3 +1,5 @@
+#include <ctype.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
@@ -12,14 +14,20 @@ void enableRawMode() {
   tcgetattr(STDIN_FILENO, &orig_termios);
   atexit(disableRawMode);
   struct termios raw = orig_termios;
-  raw.c_lflag &= ~(ECHO);
+  raw.c_lflag &= ~(ECHO | ICANON);
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 int main() {
   enableRawMode();
 
   char c;
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q'){
+    if(iscntrl(c)) {
+      printf("%d\n", c);
+    } else {
+      printf("%d ('%c')\n",c,c);
+    }
+  }
   return 0;
 }
 
